006c043d7d6103e8199b2e19aaf2f9c56755a295
Hecktor Viegas <hecktorvn@hotmail.com>
2019-09-18 10:34:53 -0300

    Add DATABASE_URL

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
-'use strict'
+'use strict';
 
 /** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use('Env')
+const Env = use('Env');
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
-const Helpers = use('Helpers')
+const Helpers = use('Helpers');
+
+const Url = require('url-parse');
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'));
 
 module.exports = {
-  /*
+	/*
   |--------------------------------------------------------------------------
   | Default Connection
   |--------------------------------------------------------------------------
@@ -16,9 +19,9 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+	connection: Env.get('DB_CONNECTION', 'pg'),
 
-  /*
+	/*
   |--------------------------------------------------------------------------
   | Sqlite
   |--------------------------------------------------------------------------
@@ -29,16 +32,16 @@ module.exports = {
   | npm i --save sqlite3
   |
   */
-  sqlite: {
-    client: 'sqlite3',
-    connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
-    },
-    useNullAsDefault: true,
-    debug: Env.get('DB_DEBUG', false)
-  },
+	sqlite: {
+		client: 'sqlite3',
+		connection: {
+			filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+		},
+		useNullAsDefault: true,
+		debug: Env.get('DB_DEBUG', false)
+	},
 
-  /*
+	/*
   |--------------------------------------------------------------------------
   | MySQL
   |--------------------------------------------------------------------------
@@ -48,19 +51,19 @@ module.exports = {
   | npm i --save mysql
   |
   */
-  mysql: {
-    client: 'mysql',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    },
-    debug: Env.get('DB_DEBUG', false)
-  },
+	mysql: {
+		client: 'mysql',
+		connection: {
+			host: Env.get('DB_HOST', 'localhost'),
+			port: Env.get('DB_PORT', ''),
+			user: Env.get('DB_USER', 'root'),
+			password: Env.get('DB_PASSWORD', ''),
+			database: Env.get('DB_DATABASE', 'adonis')
+		},
+		debug: Env.get('DB_DEBUG', false)
+	},
 
-  /*
+	/*
   |--------------------------------------------------------------------------
   | PostgreSQL
   |--------------------------------------------------------------------------
@@ -70,15 +73,15 @@ module.exports = {
   | npm i --save pg
   |
   */
-  pg: {
-    client: 'pg',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    },
-    debug: Env.get('DB_DEBUG', false)
-  }
-}
+	pg: {
+		client: 'pg',
+		connection: {
+			host: Env.get('DB_HOST', DATABASE_URL.hostname),
+			port: Env.get('DB_PORT', DATABASE_URL.port),
+			user: Env.get('DB_USER', DATABASE_URL.username),
+			password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+			database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+		},
+		debug: Env.get('DB_DEBUG', false)
+	}
+};
